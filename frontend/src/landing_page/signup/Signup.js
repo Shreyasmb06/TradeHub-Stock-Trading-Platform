3c130e56ce7b7eec3c9953fc49a485f0d7099a71
Shreyasmb06 <152626072+Shreyasmb06@users.noreply.github.com>
2025-09-17 11:15:38 +0530

    Update Signup.js

--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -32,7 +32,7 @@ const Signup = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:3002/signup",
+        "https://tradehub-stock-trading-platform.onrender.com/signup",
         {
           ...inputValue,
         },
